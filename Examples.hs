f5dd4b181e32f8e4ef07dc795b61005bd97dfdd0
Emil Axelsson <emax@chalmers.se>
2015-04-16 11:47:39 +0200

    Complete rewrite to a monadic representation The representation uses the operational monad from imperative-edsl.

--- Examples.hs
+++ Examples.hs
@@ -1,33 +1,21 @@
-{-# LANGUAGE NoMonomorphismRestriction #-}
-
 module Examples where
 
 
 
-import Language.C.Monad
-import Language.Embedded.Backend.C ()
-import Language.Embedded.Imperative (interpret, printf)
 import Language.Embedded.Expr
 
-import Frontend
-import RunIO
-import Backend
+import Zeldspar
 
 
 
-sourceIO :: IO Int
+sourceIO :: IO Float
 sourceIO = fmap (read . return) getChar
 
-sinkIO :: Int -> IO ()
+sinkIO :: Float -> IO ()
 sinkIO i = putStr (show i) >> putStr " "
 
-run :: Prog Expr Int Int a -> IO a
+run :: Z Expr Float Float a -> IO a
 run p = runIO p sourceIO sinkIO
-
-sourceComp = return 4
-sinkComp   = printf " <%d> "
-
-comp prog = prettyCGen $ wrapMain $ interpret $ compile prog sourceComp sinkComp
 
 
 
@@ -35,48 +23,48 @@ comp prog = prettyCGen $ wrapMain $ interpret $ compile prog sourceComp sinkComp
 -- * Examples
 ----------------------------------------------------------------------------------------------------
 
-prog1 :: Prog Expr Int Int ()
+prog1 :: Z Expr Float Float ()
 prog1 = do
-    r <- newRef
+    r <- newVar
     loop $ do
       receive r
-      i <- getRef r
+      i <- readVar r
       emit (i+1)
 
-prog2 :: Prog Expr Int Int ()
+prog2 :: Z Expr Float Float ()
 prog2 = do
-    r <- newRef
+    r <- newVar
     loop $ do
       receive r
-      i <- getRef r
+      i <- readVar r
       emit (i*2)
 
-run12  = run  (prog1 >>> prog2)
-comp12 = comp (prog1 >>> prog2)
+run12  = run      (prog1 >>> prog2)
+comp12 = icompile (prog1 >>> prog2)
 
-prog3 :: Prog Expr Int Int ()
+prog3 :: Z Expr Float Float ()
 prog3 = do
-    r <- newRef
+    r <- newVar
     loop $ do
       receive r
-      i <- getRef r
+      i <- readVar r
       emit (i+1)
       emit (i+2)
 
-prog4 :: Prog Expr Int Int ()
+prog4 :: Z Expr Float Float ()
 prog4 = do
-    r <- newRef
+    r <- newVar
     loop $ do
       receive r
-      i <- getRef r
+      i <- readVar r
       emit (i*2)
       receive r
-      i <- getRef r
+      i <- readVar r
       emit (i*3)
       receive r
-      i <- getRef r
+      i <- readVar r
       emit (i*4)
 
-run34  = run  (prog3 >>> prog4)
-comp34 = comp (prog3 >>> prog4)
+run34  = run      (prog3 >>> prog4)
+comp34 = icompile (prog3 >>> prog4)
 
--- Zeldspar.hs
+++ Zeldspar.hs
@@ -1,80 +1,229 @@
 {-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE ViewPatterns #-}
+
 module Zeldspar where
 
-import Language.Embedded.Imperative (VarPred, VarId)
+import Data.IORef
+
+import Language.C.Monad
+import Language.Embedded.Backend.C ()
+import Language.Embedded.Imperative
 
 infix  6 :=
-infixr 5 :>
 infixr 4 >>>
 
-data Ref a = Ref VarId
 
-data Statement exp inp out where
-  Emit    :: exp out -> Statement exp inp out
-  Receive :: VarPred exp inp => Ref inp -> Statement exp inp out
-  (:=)    :: VarPred exp a   => Ref a -> exp a -> Statement exp inp out
 
-data Program exp inp out where
-  (:>)   :: Statement exp inp out -> Program exp inp out -> Program exp inp out
-  Loop   :: Program exp inp out -> Program exp inp out
-  Return :: Program exp inp out
-  EndL   :: Program exp inp out -> Program exp inp out
+----------------------------------------------------------------------------------------------------
+-- * Representation
+----------------------------------------------------------------------------------------------------
 
-(>:) :: Program exp inp out -> Statement exp inp out -> Program exp inp out
-p >: s = p >>: (s :> Return)
+data ZeldCMD exp inp out prog a
+  where
+    NewVar  :: VarPred exp a => ZeldCMD exp inp out prog (Ref a)
+    (:=)    :: VarPred exp a => Ref a -> exp a -> ZeldCMD exp inp out prog ()
+    Emit    :: exp out -> ZeldCMD exp inp out prog ()
+    Receive :: VarPred exp inp => Ref inp -> ZeldCMD exp inp out prog ()
+    Loop    :: prog () -> ZeldCMD exp inp out prog ()
+    EndL    :: prog () -> ZeldCMD exp inp out prog ()
 
-(>>:) :: Program exp inp out -> Program exp inp out -> Program exp inp out
-(s :> p) >>: q = s :> (p >>: q)
-Loop p   >>: _ = Loop p
-Return   >>: q = q
+instance MapInstr (ZeldCMD exp inp out)
+  where
+    imap _ NewVar      = NewVar
+    imap _ (v := a)    = v := a
+    imap _ (Emit a)    = Emit a
+    imap _ (Receive r) = Receive r
+    imap f (Loop p)    = Loop (f p)
+    imap f (EndL p)    = EndL (f p)
 
-(>>>) :: Program exp inp msg -> Program exp msg out -> Program exp inp out
+type Z exp inp out = Program (ZeldCMD exp inp out)
+
+
+
+----------------------------------------------------------------------------------------------------
+-- * Interpretation
+----------------------------------------------------------------------------------------------------
+
+runZeld :: EvalExp exp => IO inp -> (out -> IO ()) -> ZeldCMD exp inp out IO a -> IO a
+runZeld src snk NewVar                = fmap RefEval $ newIORef (error "uninitialized reference")
+runZeld src snk (RefEval r := a)      = writeIORef r $ evalExp a
+runZeld src snk (Emit a)              = snk $ evalExp a
+runZeld src snk (Receive (RefEval r)) = src >>= writeIORef r
+runZeld src snk l@(Loop p)            = p >> runZeld src snk l
+
+runIO :: EvalExp exp => Z exp inp out a -> IO inp -> (out -> IO ()) -> IO a
+runIO prog src snk = interpretWithMonad (runZeld src snk) prog
+
+
+
+----------------------------------------------------------------------------------------------------
+-- * Compilation
+----------------------------------------------------------------------------------------------------
+
+compZeld
+    :: ( EvalExp (IExp instr)
+       , CompExp (IExp instr)
+       , VarPred (IExp instr) ~ IPred instr
+       , IPred instr Bool
+       , RefCMD (IPred instr) (IExp instr) :<: instr
+       , ControlCMD (IExp instr)           :<: instr
+       )
+    => Program instr (IExp instr inp)        -- ^ Source
+    -> (IExp instr out -> Program instr ())  -- ^ Sink
+    -> ZeldCMD (IExp instr) inp out (Program instr) a
+    -> Program instr a
+compZeld src snk NewVar      = newRef
+compZeld src snk (r := a)    = setRef r a
+compZeld src snk (Emit a)    = snk a
+compZeld src snk (Receive r) = src >>= setRef r
+compZeld src snk l@(Loop p)  = while (return $ litExp True) p
+
+compile
+    :: ( EvalExp (IExp instr)
+       , CompExp (IExp instr)
+       , VarPred (IExp instr) ~ IPred instr
+       , IPred instr Bool
+       , RefCMD (IPred instr) (IExp instr) :<: instr
+       , ControlCMD (IExp instr)           :<: instr
+       )
+    => Z (IExp instr) inp out a
+    -> Program instr (IExp instr inp)        -- ^ Source
+    -> (IExp instr out -> Program instr ())  -- ^ Sink
+    -> Program instr a
+compile prog src snk = interpretWithMonad (compZeld src snk) prog
+
+icompile
+    :: forall instr exp a . ( EvalExp exp
+       , CompExp exp
+       , VarPred exp Bool
+       , VarPred exp Float
+       , instr ~ (RefCMD (VarPred exp) exp :+: ControlCMD exp :+: FileCMD exp)
+       )
+    => Z exp Float Float a -> IO ()
+icompile prog = print =<< prettyCGen (wrapMain $ interpret cprog)
+  where
+    cprog = do
+        inp <- open "stdin"
+        out <- open "stdout"
+        compile prog (fget inp) (fput out) :: Program instr a
+
+
+
+----------------------------------------------------------------------------------------------------
+-- * Pipelining
+----------------------------------------------------------------------------------------------------
+
+pattern Ret a   <- (view -> Return a)
+pattern i :>= p <- (view -> i :>>= p)
+
+(|>=) :: instr (Program instr) a -> (a -> Program instr b) -> Program instr b
+i |>= p = singleton i >>= p
+  -- TODO It should be possible to use `:>=` for this using `where` syntax. Maybe that only works in
+  --      GHC 7.10?
+
+(|>>) :: instr (Program instr) a -> Program instr b -> Program instr b
+i |>> p = singleton i >> p
+
+(>>|) :: Program instr a -> instr (Program instr) b -> Program instr b
+p >>| i = p >> singleton i
+
+-- TODO Generalize result type of >>>
+
+(>>>) :: Z exp inp msg () -> Z exp msg out () -> Z exp inp out ()
 
 -- termination
-Return >>> q      = Return
-p      >>> Return = Return
+(Ret a) >>> _       = return a
+_       >>> (Ret b) = return b
 
--- connect!
-(Emit m :> p) >>> (Receive v :> q) = (v := m) :> (p >>> q)
+-- variables
+(NewVar :>= p) >>> q              = NewVar |>= \v -> p v >>> q
+p              >>> (NewVar :>= q) = NewVar |>= \v -> p >>> q v
 
-((v := e) :> p) >>> q               = (v := e) :> (p >>> q)
-p               >>> ((v := e) :> q) = (v := e) :> (p >>> q)
+((v := a) :>= p) >>> q                = (v := a) |>> (p () >>> q)
+p                >>> ((v := a) :>= q) = (v := a) |>> (p >>> q ())
 
--- loop...
-Loop p >>> Loop q = Loop ((p >>: EndL p) >>> (q >>: EndL q))
+-- connect
+(Emit m :>= p) >>> (Receive v :>= q) = (v := m) |>> (p () >>> q ())
 
-Loop Return >>> q = blockInp q
-Loop p      >>> q = unloop p >>> q
+-- loop
+(Loop p :>= _) >>> (Loop q :>= _) = singleton $ Loop ((p >>| EndL p) >>> (q >>| EndL q))
 
-p >>> Loop Return = blockOut p
-p >>> Loop q      = p >>> unloop q
+(Loop (Ret _) :>= _) >>> q = blockInp q
+(Loop p       :>= _) >>> q = unloop p >>> q
+
+p >>> (Loop (Ret _) :>= _) = blockOut p
+p >>> (Loop q       :>= _) = p >>> unloop q
 
 -- outside actions
-(Receive v :> p) >>> q             = Receive v :> (p >>> q)
-p                >>> (Emit m :> q) = Emit m :> (p >>> q)
+(Receive v :>= p) >>> q              = Receive v |>> (p () >>> q)
+p                 >>> (Emit m :>= q) = Emit m    |>> (p >>> q ())
 
 -- end loop
-EndL _ >>> EndL _ = Return
-EndL p >>> q      = (p >>: EndL p) >>> q
-p      >>> EndL q = p >>> (q >>: EndL q)
+(EndL _ :>= _) >>> (EndL _ :>= _) = return ()
+(EndL p :>= _) >>> q              = (p >>| EndL p) >>> q
+p              >>> (EndL q :>= _) = p >>> (q >>| EndL q)
 
-unloop :: Program exp inp out -> Program exp inp out
-unloop (s :> p) = s :> Loop (p >: s)
-unloop (Loop p) = unloop p
+unloop :: Z exp inp out () -> Z exp inp out ()
+unloop (NewVar        :>= q) = NewVar |>= \v -> singleton (Loop (q v))  -- No need for NewVar at the end
+unloop (i@(_ := _)    :>= q) = i |>> singleton (Loop (q () >>| i))
+unloop (i@(Emit _)    :>= q) = i |>> singleton (Loop (q () >>| i))
+unloop (i@(Receive _) :>= q) = i |>> singleton (Loop (q () >>| i))
+unloop (Loop q        :>= _) = unloop q
+  -- TODO It's assumed that `p /= Ret a`. This could be captured in the type.
 
-blockInp :: Program exp inp out -> Program exp xxx out
-blockInp (Receive _ :> _) = Loop Return
-blockInp (Emit m :> p)    = Emit m :> blockInp p
-blockInp ((v := e) :> p)  = (v := e) :> blockInp p
-blockInp (Loop p)         = Loop (blockInp p)
-blockInp Return           = Return
-blockInp (EndL p)         = blockInp (Loop p)
+blockInp :: Z exp inp out () -> Z exp xxx out ()
+blockInp (NewVar    :>= p) = NewVar    |>= \v -> blockInp (p v)
+blockInp ((v := e)  :>= p) = (v := e)  |>> blockInp (p ())
+blockInp (Emit m    :>= p) = Emit m    |>> blockInp (p ())
+blockInp (Receive _ :>= _) = singleton $ Loop (return ())
+blockInp (Loop p    :>= _) = singleton $ Loop (blockInp p)
+blockInp (EndL p    :>= q) = blockInp (Loop p |>= q)
+blockInp (Ret a)           = return a
 
-blockOut :: Program exp inp out -> Program exp inp xxx
-blockOut (Emit _ :> _)    = Loop Return
-blockOut (Receive v :> p) = Receive v :> blockOut p
-blockOut ((v := e) :> p)  = (v := e) :> blockOut p
-blockOut (Loop p)         = Loop (blockOut p)
-blockOut Return           = Return
-blockOut (EndL p)         = blockOut (Loop p)
+blockOut :: Z exp inp out () -> Z exp inp xxx ()
+blockOut (NewVar    :>= p) = NewVar    |>= \v -> blockOut (p v)
+blockOut ((v := e)  :>= p) = (v := e)  |>> blockOut (p ())
+blockOut (Emit _    :>= _) = singleton $ Loop (return ())
+blockOut (Receive v :>= p) = Receive v |>> blockOut (p ())
+blockOut (Loop p    :>= _) = singleton $ Loop (blockOut p)
+blockOut (EndL p    :>= q) = blockOut (Loop p |>= q)
+blockOut (Ret a)           = return a
+
+
+
+----------------------------------------------------------------------------------------------------
+-- * Front end
+----------------------------------------------------------------------------------------------------
+
+newVar :: VarPred exp a => Z exp inp out (Ref a)
+newVar = singleton NewVar
+
+initVar :: VarPred exp a => exp a -> Z exp inp out (Ref a)
+initVar a = do
+    v <- newVar
+    v =: a
+    return v
+
+(=:) :: VarPred exp a => Ref a -> exp a -> Z exp inp out ()
+v =: a = singleton (v := a)
+
+readVar :: (VarPred exp a, EvalExp exp, CompExp exp) => Ref a -> Z exp inp out (exp a)
+readVar v = do
+    w <- newVar
+    w =: unsafeFreezeRef v
+    return $ unsafeFreezeRef w
+
+emit :: exp out -> Z exp inp out ()
+emit = singleton . Emit
+
+receive :: VarPred exp inp => Ref inp -> Z exp inp out ()
+receive = singleton . Receive
+
+loop :: Z exp inp out () -> Z exp inp out ()
+loop = singleton . Loop
+
