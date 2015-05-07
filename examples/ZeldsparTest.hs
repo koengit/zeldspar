d76ec19cfa9b0bf9c319165bb33c548e1f2aaae6
Emil Axelsson <emax@chalmers.se>
2015-05-07 15:35:15 +0200

    An example that does some imperative stuff around the Zeldspar program

--- examples/ZeldsparTest.hs
+++ examples/ZeldsparTest.hs
@@ -4,8 +4,9 @@ module ZeldsparTest where
 
 import Prelude ()
 
-import Feldspar
+import Feldspar hiding (getRef, modifyRef)
 import Feldspar.SimpleVector
+import Feldspar.IO hiding (compile)
 
 import Zeldspar
 
@@ -25,6 +26,24 @@ prog2 = loop $ do
     j <- receive
     emit (i*j*3333)
 
-test1_1 = compile prog1
+delay = do
+    ir <- initRef (0 :: Data Word32)
+    while (fmap (<100000) $ getRef ir) $ modifyRef ir (+1)
+
+prog3 = do
+    inp <- fopen "input" ReadMode
+    out <- fopen "output" WriteMode
+    printf "files open, starting to loop..."
+    translate (prog1 >>> prog2) (fget' inp) (fprintf out "%d ")
+  where
+    fget' h = do
+        delay  -- The program is an infinite loop so it has to be slowed down.
+               -- Otherwise it will quickly produce a gigantic file.
+        end <- feof h
+        return 0
+        ifE end (return 0) (fget h)
+
+test1   = compile prog1
 test1_2 = compile (prog1 >>> prog2)
+test3   = Feldspar.IO.icompile prog3
 
