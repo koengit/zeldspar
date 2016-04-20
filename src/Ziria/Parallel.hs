29b0f4204dbc9ca9442a5b80b3e268ff458eaad0
Karácsony Máté <k_mate@inf.elte.hu>
2016-04-20 21:53:47 +0200

    Representation and front end for parallel Ziria programs

---
+++ src/Ziria/Parallel.hs
@@ -0,0 +1,33 @@
+-- | Parallel stream composition for Ziria
+module Ziria.Parallel where
+
+import Ziria
+
+
+--------------------------------------------------------------------------------
+-- * Representation
+--------------------------------------------------------------------------------
+
+data ParZ inp out m a
+  = LiftP (Z inp out m ())
+  | forall mid. (ParZ inp mid m ()) :|>>>| (ParZ mid out m ())
+
+
+--------------------------------------------------------------------------------
+-- * Front end
+--------------------------------------------------------------------------------
+
+class Parallel p where
+  -- | Lift a computation to the parallel level.
+  liftP :: p inp out m () -> ParZ inp out m ()
+
+instance Parallel ParZ where
+  liftP = id
+
+instance Parallel Z where
+  liftP = LiftP
+
+(|>>>|) :: (Parallel a, Parallel b, Monad m)
+        => a inp mid m () -> b mid out m () -> ParZ inp out m ()
+l |>>>| r = liftP l :|>>>| liftP r
+
