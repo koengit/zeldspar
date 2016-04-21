5c3150bea91e3c3cd85a620ac46820faccd9e616
Karácsony Máté <k_mate@inf.elte.hu>
2016-04-21 15:54:48 +0200

    Omitting return value in translate + using unsafeFreezeStore in store

--- src/Zeldspar.hs
+++ src/Zeldspar.hs
@@ -11,24 +11,20 @@ import Ziria
 
 type Zun inp out = Z inp out Run
 
--- TODO: should it return Run ()? then end = return ()
-translate :: forall inp out a. Zun inp out a
+translate :: forall inp out a. Zun inp out ()
           -> (Run inp)        -- ^ Source
           -> (out -> Run ())  -- ^ Sink
-          -> Run a
+          -> Run ()
 translate (Z p) src snk = trans (p (\_ -> Stop))
   where
-    trans :: Action inp out Run -> Run a
+    trans :: Action inp out Run -> Run ()
     trans (Lift rp)     = rp >>= \a -> trans a
     trans (Emit x p)    = snk x >> trans p
     trans (Receive p)   = src >>= trans . p
-    trans Stop          = end
-    trans (Loop p)      = while (return true) (void $ trans p) >> end
+    trans Stop          = return ()
+    trans (Loop p)      = while (return true) (void $ trans p)
     trans (Times n p k) = for (0, 1, Excl $ value n) (const $ void $ trans p)
                        >> trans k
-    
-    end :: Run a
-    end = return $ error "unreachable"
 
 
 --------------------------------------------------------------------------------
@@ -39,7 +35,5 @@ store :: Storable a => Zun a a ()
 store = loop $ do
     i <- receive
     s <- lift $ initStore i
-    o <- lift $ readStore s
--- TODO: what about unsafeFreezeStore instead of readStore?
+    o <- lift $ unsafeFreezeStore s
     emit o
-
