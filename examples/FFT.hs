2046d0f86ade18f2132b6d6a782c42bd056c5515
Karácsony Máté <k_mate@inf.elte.hu>
2016-04-27 00:53:01 +0200

    Completing FFT example

--- examples/FFT.hs
+++ examples/FFT.hs
@@ -6,6 +6,14 @@ import Zeldspar
 import Zeldspar.Parallel
 
 
+type Samples  = Vector (Data (Complex Double))
+type Twiddles = Vector (Data (Complex Double))
+
+
+--------------------------------------------------------------------------------
+-- Bit reversal
+--------------------------------------------------------------------------------
+
 -- | Rotates the 'k + 1' LSB bits right with 1.
 rotBit :: Data Index -> Data Index -> Data Index
 rotBit k i = lefts .|. rights
@@ -32,15 +40,36 @@ bitRevPar n = foldl1 (|>>>|) [ liftP $ riffle $ value k | k <- [1..n'] ]
     n' = floor (logBase 2 $ fromIntegral n) - 1
 
 --------------------------------------------------------------------------------
+-- "Push-style" FFT
+--------------------------------------------------------------------------------
 
--- | Indicates whether the 'i'th bit of 'a' is set.
-testBit :: (Bits a, Num a, PrimType a) => Data a -> Data Index -> Data Bool
-testBit a i = a .&. (1 .<<. i2n i) /= 0
+fft :: Length
+    -> Zun Samples Samples ()
+fft n = fftCore n False >>> store >>> bitRev n
+
+fftPar :: Length
+       -> ParZun Samples Samples ()
+fftPar n = fftCorePar n False |>>>| bitRev n
+
+-- | Performs all 'ilog2 n' FFT/IFFT stages on a sample vector.
+fftCore :: Length
+        -> Bool  -- ^ Inverse?
+        -> Zun Samples Samples ()
+fftCore n inv = foldl1 (>>>) [ step inv $ value k | k <- Prelude.reverse [0..n'] ]
+  where
+    n' = floor (logBase 2 $ fromIntegral n) - 1
+
+fftCorePar :: Length
+        -> Bool  -- ^ Inverse?
+        -> ParZun Samples Samples ()
+fftCorePar n inv = foldl1 (|>>>|) [ liftP $ step inv $ value k | k <- Prelude.reverse [0..n'] ]
+  where
+    n' = floor (logBase 2 $ fromIntegral n) - 1
 
 -- | Performs the 'k'th FFT/IFFT stage on a sample vector.
 step :: Bool
      -> Data Length
-     -> Zun (Vector (Data (Complex Double))) (Vector (Data (Complex Double))) ()
+     -> Zun Samples Samples ()
 step inv k = do
     v <- receive
     let ixf i = testBit i k ? (twid * (b - a)) $ (a + b)
@@ -52,58 +81,42 @@ step inv k = do
             k2   = 1 .<<. k'
     emit $ Indexed (length v) ixf
 
--- | Performs all 'ilog2 n' FFT/IFFT stages on a sample vector.
-fftCore :: Length
-        -> Bool  -- ^ Inverse?
-        -> Zun (Vector (Data (Complex Double))) (Vector (Data (Complex Double))) ()
-fftCore n inv = foldl1 (>>>) [ step inv $ value k | k <- Prelude.reverse [0..n'] ]
-  where
-    n' = floor (logBase 2 $ fromIntegral n) - 1
-
-fftCorePar :: Length
-        -> Bool  -- ^ Inverse?
-        -> ParZun (Vector (Data (Complex Double))) (Vector (Data (Complex Double))) ()
-fftCorePar n inv = foldl1 (|>>>|) [ liftP $ step inv $ value k | k <- Prelude.reverse [0..n'] ]
-  where
-    n' = floor (logBase 2 $ fromIntegral n) - 1
+-- | Indicates whether the 'i'th bit of 'a' is set.
+testBit :: (Bits a, Num a, PrimType a) => Data a -> Data Index -> Data Bool
+testBit a i = a .&. (1 .<<. i2n i) /= 0
 
 --------------------------------------------------------------------------------
-
-fft :: Length
-    -> Zun (Vector (Data (Complex Double))) (Vector (Data (Complex Double))) ()
-fft n = fftCore n False >>> store >>> bitRev n
-
-fftPar :: Length
-       -> ParZun (Vector (Data (Complex Double))) (Vector (Data (Complex Double))) ()
-fftPar n = fftCorePar n False |>>>| bitRev n
-
+-- "Pull-style" FFT
 --------------------------------------------------------------------------------
 
--- Work in progress ideas / drafts
+fft' n = (foldl1 (>>>) [ mkStage s | s <- [0..n'] ]) >>> bitRev 8
+  where
+    n' = floor (logBase 2 $ fromIntegral n) - 1
+    mkStage :: Length -> Zun Samples Samples ()
+    mkStage s =
+        let tw = permute (\_ i -> i .<<. (i2n $ value s)) (twiddles 8)
+        in  chunk (2 ^ s) (butterfly tw) >>> store
 
-fft8 = do
-    v <- receive
-    tw8 <- lift $ force $ twiddles 8
-    let evens = permute $ \_ i -> i .<<. 1
-        tw4 = evens tw8
-        tw2 = evens tw4
-        stage0 = chunk 1 (butterfly tw8)
-        stage1 = chunk 2 (butterfly tw4)
-        stage2 = chunk 4 (butterfly tw2)
-    stage0 >>> store >>> stage1 >>> store >>> stage2 >>> store >>> bitRev 8
+fftPar' n = (foldl1 (|>>>|) [ liftP $ mkStage s | s <- [0..n'] ]) |>>>| bitRev 8
+  where
+    n' = floor (logBase 2 $ fromIntegral n) - 1
+    mkStage :: Length -> Zun Samples Samples ()
+    mkStage s =
+        let tw = permute (\_ i -> i .<<. (i2n $ value s)) (twiddles 8)
+        in  chunk (2 ^ s) (butterfly tw)
 
 -- | Twiddle factors for an `n`-point FFT.
-twiddles :: Length -> Vector (Data (Complex Double))
+twiddles :: Length -> Twiddles
 twiddles n = Indexed (value n .>>. 1)
                      (\i -> polar 1 $ -π * 2 * i2n i / (i2n $ value n))
 
 -- | A variable-width butterfly using the given twiddle factors.
-butterfly :: Vector (Data (Complex Double))
-          -> Vector (Data (Complex Double))
-          -> Run (Vector (Data (Complex Double)))
-butterfly twiddles v = do
-    let half = length v .>>. 1
-        (lower, upper) = splitAt half v
+butterfly :: Twiddles
+          -> Samples
+          -> Run Samples
+butterfly twiddles samples = do
+    let half = length samples .>>. 1
+        (lower, upper) = splitAt half samples
     (left, right) <- force $ unzip $ zipWith dft2 twiddles $ zip lower upper
     return $ left +++ right
 
@@ -113,9 +126,8 @@ dft2 twiddle (a, b) = (a + b, twiddle * (a - b))
 
 -- | Processing a vector in chunks.
 chunk :: Length
-      -> (Vector (Data (Complex Double))
-          -> Run (Vector (Data (Complex Double))))
-      -> Zun (Vector (Data (Complex Double))) (Vector (Data (Complex Double))) ()
+      -> (Samples -> Run Samples)
+      -> Zun Samples Samples ()
 chunk chunks p = do
     v <- receive
     let len = length v `div` (value chunks)
@@ -136,36 +148,8 @@ a +++ b = Indexed (aLen + bLen) $ \i -> (i < aLen) ? (a ! i) $ (b ! (i - aLen))
     aLen = length a
     bLen = length b
 
-{-
-halve :: Type a => Zun (Vector (Data a)) (Vector (Data a)) ()
-halve = do
-    v <- receive
-    let n2 = length v .>>. 1
-    emit $ take n2 v
-    emit $ drop n2 v
-
-unhalve :: Type a => Zun (Vector (Data a)) (Vector (Data a)) ()
-unhalve = do
-    v1 <- receive
-    v2 <- receive
-    emit $ v1 +++ v2
-
--- | Two-point DFT butterfly for Decimation-In-Frequency
-dft2 :: Data (Complex Double)
-     -> Zun (Vector (Data (Complex Double))) (Vector(Data (Complex Double))) ()
-dft2 twiddle = do
-    v <- receive
-    let a = v ! 0
-        b = v ! 1
-        v' = Indexed 2 f
-        f i = i /= 1 ? (twiddle * (b - a)) $ (a + b)
-    lift $ printf "dft2 (twiddle %f %f) (A: %f %f) (B: %f %f)\n"
-        (realPart twiddle) (imagPart twiddle)
-        (realPart a) (imagPart a)
-        (realPart b) (imagPart b)
-    emit v'
--}
-
+--------------------------------------------------------------------------------
+-- Testing utilities
 --------------------------------------------------------------------------------
 
 test :: Zun (Vector (Data (Complex Double))) (Vector (Data (Complex Double))) ()
